bbc918bd6ee49a14e568da67abcd3b22f3e94fc9
unknown <carthman321@gmail.com>
2024-12-30 23:40:20 -0600

    login

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -10,29 +10,6 @@
     <title>Document</title>
 </head>
 <body class="bg-gradient-to-r from-[#072E33] from-10% via-[#0C7075] via-30% to-[#0F969C] to-90%  flex items-center justify-center h-screen">
-    <div class="max-w-[1000px] bg-[#00000050]"> 
-        <div class="justify-center items-center">
-            <h1>Logueate</h1>
-            <form action="">
-                <div>
-                    <div class=" ">
-                        <i class="fa-solid fa-envelope"></i>
-                    </div>
-                    {{-- <input type="email" placeholder="Usuario"> --}}
-
-                    <div>
-                        <i class="fa-solid fa-lock"></i>
-                    </div>
-                    <input type="text" placeholder="Contraseña">
-                </div>
-                <button class="btn btn-outline btn-secondary">Iniciar secion</button>
-            </form>
-            <div>
-                <p>
-                    ¿Olvido la contraseña?<a href="Registrate">Recuperar</a>
-                </p>
-            </div>
-        </div>
-    </div>
+    @yield('contenido')
 </body>
 </html>
