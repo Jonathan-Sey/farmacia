9e9399b3bbed3b532f8559d755d9e693e5d1c97c
unknown <carthman321@gmail.com>
2025-01-03 10:31:53 -0600

    integracion de ruta a dashboard

--- resources/views/components/navegacion.blade.php
+++ resources/views/components/navegacion.blade.php
@@ -1,7 +1,7 @@
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">
-                    <a href="/">Farmacia</a>
+                    <a href="/dashboard">Farmacia</a>
                 </h1>
                 <div class="flex gap-2 items-center">
                     {{-- <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Usuarios</a> --}}
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Login.login');
 });
-Route::get('/', function () {
-    return view('panel.index');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
 });
 // //modulo rol
 // Route::get('/roles', [RolController::class, 'index'] )->name('roles');
