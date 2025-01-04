f40915383da54fe81509ed2fb7cb801cc29ed9b9
unknown <carthman321@gmail.com>
2025-01-03 18:29:02 -0600

    crear producto y acciones edit y delete

--- app/Http/Controllers/Producto/ProductoController.php
+++ app/Http/Controllers/Producto/ProductoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Producto;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categoria;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 
@@ -29,7 +30,8 @@ class ProductoController extends Controller
      */
     public function create()
     {
-        return view('producto.create');
+        $categorias = Categoria::all(['id', 'nombre']);
+        return view('producto.create', compact('categorias'));
     }
 
     /**
@@ -40,7 +42,32 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            // 'codigo'=>['nullable'],
+            'id_categoria'=>'required',
+            'nombre'=>['required','string','max:50'],
+            'descripcion'=>['max:100'],
+            'precio_venta'=>'integer|required',
+            'fecha_caducidad'=>'required',
+            'estado'=>'integer',
+        ]);
+        // generacion de codigo
+        $codigo = 'P' . $request->id_categoria . '-' . (Producto::max('id') + 1);
+
+        $producto = Producto::create([
+            'nombre' => $request->nombre,
+            'descripcion' => $request->descripcion,
+            'precio_venta' => $request->precio_venta,
+            'fecha_caducidad' => $request->fecha_caducidad,
+            'id_categoria' => $request->id_categoria,
+            'estado' => 1,
+            'codigo' => $codigo, // asignamos el codigo generado
+        ]);
+
+
+        $producto->update(['codigo'=>$codigo]);
+        return redirect()->route('productos.index')->with('success','¡Registro exitoso!');
+
     }
 
     /**
