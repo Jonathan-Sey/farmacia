0f21fc51d34d42c121e2fe5ebd64cf7fc6c37de6
unknown <carthman321@gmail.com>
2025-01-04 16:53:40 -0600

    modulo proveedores finalizado

---
+++ database/migrations/2025_01_04_194526_create_proveedors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProveedorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedor', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',35)->unique();
+            $table->string('telefono',20);
+            $table->string('empresa',35);
+            $table->string('correo',35);
+            $table->text('direccion')->nullable();;
+            $table->tinyInteger('estado')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedor');
+    }
+}
