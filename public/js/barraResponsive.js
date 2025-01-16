4402f25d0b75280d5c77659bc5cf2d491fa26bbb
soulbreaker156 <soulbreaker0220@gmail.com>
2025-01-16 16:46:09 -0600

    arreglo de la barra responsive

--- public/js/barraResponsive.js
+++ public/js/barraResponsive.js
@@ -6,12 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
     if (menuBtn && menu) {
   
         menuBtn.addEventListener('click', function() {
-            console.log('Botón de menú clickeado'); 
+            console.log('Botón de menú clickeado');  
             menu.classList.toggle('hidden'); 
             
-        });
-    
-
 
     const token = localStorage.getItem('jwt_token');
     console.log(token);
@@ -21,18 +18,32 @@ document.addEventListener('DOMContentLoaded', function () {
         
         setupNavigation(pestanas);
     }
+});
+
+window.addEventListener('resize', () => { 
+    const menu = document.getElementById('menu'); 
+    if (window.innerWidth >= 768) { menu.classList.add('hidden'); } 
+});
 }
+
+
     // Función para configurar la navegación dinámica según las pestañas
     function setupNavigation(pestanas) {
-        const navItems = document.querySelectorAll('[data-pestana]');
+        
+       const navItems = document.querySelectorAll('[data-pestanal]');
         navItems.forEach(item => item.style.display = 'none'); // Ocultar todas las pestañas
 
         pestanas.forEach(pestana => {
-            const navItem = document.querySelector(`[data-pestana="${pestana}"]`);
+            var nev;
+            const navItem = document.querySelector(`[data-pestanal="${pestana}"]`);
             if (navItem) {
                 navItem.style.display = 'block'; // Mostrar las pestañas correspondientes
+                
             }
+          
         });
     }
 });
+
+
 
