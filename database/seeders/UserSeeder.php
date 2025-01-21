256d7d4e67cb7b838b9dc3891d518adf68341a12
soulbreaker156 <soulbreaker0220@gmail.com>
2025-01-20 19:20:16 -0600

    Arreglo de la barra de navegacion y mas cosas

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => null,
+            'password' => Hash::make('admin123'), // Cambia 'password' por la contraseña que desees
+            'id_rol' => 1,
+            'remember_token' => null,
+            'activo' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
