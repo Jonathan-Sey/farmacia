96fd4b1fd8e3c58dce6c49d83cb29717d423b075
soulbreaker156 <soulbreaker0220@gmail.com>
2025-01-24 23:48:30 -0600

    Agregado el alert de error al iniciar secion(aun falta arreglar unas cosas) y otros ajustes

--- public/js/controlIniciodesecion.js
+++ public/js/controlIniciodesecion.js
@@ -1,6 +1,6 @@
 document.getElementById('login-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-
+    e.preventDefault(); // Evitar que el formulario se envíe de manera tradicional
+    
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
@@ -8,16 +8,45 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
         email: email,
         password: password,
     })
-        .then(response => {
-            const token = response.data.token;
+    .then(response => {
+        const token = response.data.token;
 
-            // Guardar el token en localStorage
-            localStorage.setItem('jwt_token', token);
+        // Guardar el token en localStorage
+        localStorage.setItem('jwt_token', token);
 
-            // Redirigir al dashboard
+        // Redirigir al dashboard
+        Swal.fire({
+            icon: 'success',
+            title: '¡Éxito!',
+            text: 'Inicio de sesión exitoso. Redirigiendo...',
+            position: 'top',  // Posición en la parte superior de la pantalla
+            customClass: {
+                popup: 'fixed top-10 left-1/2 transform -translate-x-1/2 z-50', // Posición fija y centrada
+            },
+            didOpen: () => {
+                document.body.classList.add('overflow-hidden');  // Desactivar el scroll mientras el modal está abierto
+            },
+            willClose: () => {
+                document.body.classList.remove('overflow-hidden');  // Restaurar el scroll cuando se cierre el modal
+            }
+        }).then(() => {
             window.location.href = '/dashboard';
-        })
-        .catch(error => {
-            alert('Error al iniciar sesión. Verifique sus credenciales.');
         });
+    })
+    .catch(error => {
+        Swal.fire({
+            icon: 'error',
+            title: '¡Error!',
+            text: 'Credenciales incorrectas o hubo un error al intentar iniciar sesión.',
+            customClass: {
+                popup: 'fixed top-10 left-1/2 transform -translate-x-1/2 z-50',
+            },
+            didOpen: () => {
+                document.body.classList.add('overflow-hidden');  // Desactivar el scroll mientras el modal está abierto
+            },
+            willClose: () => {
+                document.body.classList.remove('overflow-hidden');  // Restaurar el scroll cuando se cierre el modal
+            }
+        });
+    });
 });
