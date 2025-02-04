8d4356df4877a493e4089c5e5d3301ebb07cdbc3
unknown <carthman321@gmail.com>
2025-02-03 18:43:05 -0600

    tabla lote, inventario, migraciones y modelos

---
+++ app/Models/Inventario.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inventario extends Model
+{
+    use HasFactory;
+    protected $table = 'inventario';
+
+    protected $fillable = [
+        'id_producto',
+        'id_sucursal',
+        'id_lote',
+        'cantidad',
+    ];
+
+     // Relación con la tabla `producto`
+     public function producto()
+     {
+         return $this->belongsTo(Producto::class, 'id_producto');
+     }
+
+     // Relación con la tabla `sucursal`
+     public function sucursal()
+     {
+         return $this->belongsTo(Sucursal::class, 'id_sucursal');
+     }
+
+     // Relación con la tabla `lote`
+     public function lote()
+     {
+         return $this->belongsTo(Lote::class, 'id_lote');
+     }
+}
---
+++ app/Models/Lote.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lote extends Model
+{
+    use HasFactory;
+
+    protected $tabla = 'lote';
+    protected $fillable = [
+        'id_producto',
+        'numero_lote',
+        'fecha_vencimiento',
+        'cantidad',
+        'id_compra',
+    ];
+
+      // Relación con la tabla producto
+      public function producto()
+      {
+          return $this->belongsTo(Producto::class, 'id_producto');
+      }
+
+      // Relación con la tabla compra
+      public function compra()
+      {
+          return $this->belongsTo(Compra::class, 'id_compra');
+      }
+
+      // Relación con la tabla inventario
+      public function inventarios()
+      {
+          return $this->hasMany(Inventario::class, 'id_lote');
+      }
+
+      // Relación con la tabla lote
+    public function lotes()
+    {
+        return $this->hasMany(Lote::class, 'id_compra');
+    }
+}
