d827be8df7d3c303fcfcbefa9c17646f8c6eb261
FrederS2 <alva59431@gmail.com>
2025-03-02 10:41:37 -0600

    cabio login

--- public/js/obtenerUsuario.js
+++ public/js/obtenerUsuario.js
@@ -6,25 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {function obtenerUsuar
         alert("No se encontró un token activo");
         return;
     }
-<<<<<<< HEAD
-    
-    const decodificarToken = jwt_decode(token);
-    const limiteDeTiempo = Date.now() / 1000;
-  
-        //Axios incluira el token en todas las peticiones
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-        // Configurar las pestañas del menú
-        const userName = decodificarToken.id;
-        console.log(userName); 
-
-      crearUsuario.innerHTML = ` <input type="text" class="form-control" id="idUsuario" name="idUsuario" value="${userName}" hidden>`; 
-
-
-}
-
-obtenerUsuario();})
-=======
 
     const decodificarToken = jwt_decode(token);
 
@@ -41,4 +22,4 @@ obtenerUsuario();})
 }
 
 obtenerUsuario();})
->>>>>>> main
+
