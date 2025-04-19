9b6d0c2de160273b69f028f8e783a0b6d34aa616
Frede44 <falval@miumg.edu.gt>
2025-04-19 10:41:45 -0600

    enviar correo

---
+++ resources/views/emails/verificacion.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verificacion de devoluciones</title>
+</head>
+<body>
+    <h1>aceptas estas devoluciones </h1>
+  <a href="{{ route('devoluciones.aceptar'), }}">Aceptar</a>
+</body>
+</html>
