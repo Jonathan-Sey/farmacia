a7f53f8d102c93852bd3d587bc36f997c1a04008
ErickaBm11 <erickabatzibal25@gmail.com>
2025-05-13 15:20:15 -0600

    ficha_medica_arreglada

--- resources/views/fichas/create.blade.php
+++ resources/views/fichas/create.blade.php
@@ -3,6 +3,16 @@
 @section('contenido')
 <div class="flex justify-center items-center mx-3">
     <div class="bg-white p-5 rounded-xl shadow-lg w-full max-w-3xl mb-10">
+        @if ($errors->any())
+    <div class="mb-4 text-red-600">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
         <form action="{{ route('fichas.store', $persona->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
 
@@ -14,6 +24,21 @@
                     name="diagnostico"
                     id="diagnostico"
                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">{{ old('diagnostico') }}</textarea>
+            </div>
+            <div class="mt-2 mb-5">
+                <label for="detalle_medico_id" class="uppercase block text-sm font-medium text-gray-900">
+                    Médico
+                </label>
+                <select name="detalle_medico_id" id="detalle_medico_id" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm" required>
+                    <option value="" disabled selected>-- Seleccione un médico --</option>
+                    @foreach($medicos as $detalle)
+                      <option value="{{ $detalle->id }}"
+                          {{ old('detalle_medico_id') == $detalle->id ? 'selected' : '' }}>
+                        {{ $detalle->usuario->name }} {{-- o el campo que uses para el nombre --}}
+                        @if($detalle->especialidad) – {{ $detalle->especialidad }} @endif
+                      </option>
+                    @endforeach
+                  </select>
             </div>
 
             <div class="mt-2 mb-5">
@@ -37,6 +62,8 @@
                 </a>
                 <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600">Guardar</button>
             </div>
+
+            
         </form>
     </div>
 </div>
